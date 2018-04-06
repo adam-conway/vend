5ba0df9d8350597e2cfc6941587d83ff9d4bca5b
adam-conway <adam.n.conway@gmail.com>
2018-04-06 10:20:07 -0600

    Finished first user story

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,3 +1,5 @@
 class Machine < ApplicationRecord
   belongs_to :owner
+  has_many :machine_snack
+  has_many :snacks, through: :machine_snack
 end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,21 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+  scenario 'they see the names and prices of all snacks associated with machine' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack1 = Snack.create!(name: "Hershey", price: 1.50)
+    snack2 = Snack.create!(name: "snickers", price: 2.50)
+    snack3 = Snack.create!(name: "twix", price: 4.50)
+    MachineSnack.create!(machine_id: dons.id, snack_id: snack1.id)
+    MachineSnack.create!(machine_id: dons.id, snack_id: snack2.id)
+    MachineSnack.create!(machine_id: dons.id, snack_id: snack3.id)
+
+    visit machine_path(dons)
+    save_and_open_page
+
+    expect(page).to have_content("Hershey: $1.50")
+    expect(page).to have_content("snickers: $2.50")
+    expect(page).to have_content("twix: $4.50")
+  end
 end
